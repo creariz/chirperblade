7ed1fb16f31b3951be1b5380088ffdfad0a123a1
Jesús Fernando Carrasco Mendoza <jefecame@hotmail.com>
2023-07-19 21:26:25 -0700

    Nuevo index Blade View

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index():View
     {
-        //
+        return view('chirps.index');
     }
 
     /**
